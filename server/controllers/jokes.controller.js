f77f296345472c787b82e508e01395f2cb9ecec4
ChanRithy27 <chanrithy27@icloud.com>
2023-07-16 22:00:09 -0700

    Finished creating exports in the jokes.controler file.

--- server/controllers/jokes.controller.js
+++ server/controllers/jokes.controller.js
@@ -20,4 +20,36 @@ modules.exports.findOneJoke = (req, res) => {
         });
 }
 
-modules.exports.
+modules.exports.createNewJoke = (req, res) => {
+    Joke.create(req.body)
+        .then(newlyCreatedJoke => {
+            res.json({ joke: newlyCreatedJoke });
+        })
+        .catch((err) => {
+            res.json({ message: 'Something went wrong creating the joke', error: err });
+        });
+}
+
+modules.exports.updateExistingJoke = (req, res) => {
+    Joke.findJokeAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true, runValidators: true }
+    )
+        .then(updatedJoke => {
+            res.json({ joke: updatedJoke });
+        })
+        .catch((err) => {
+            res.json({ message: 'Something went wrong updating the joke', error: err });
+        });
+}
+
+module.exports.deleteExistingJoke = (req, res) => {
+    Joke.deleteOne({ _id: req.params.id })
+    .then(result => {
+        res.json({ result: result });
+    })
+    .catch((err) => {
+        res.json({ message: 'Somethign went wrong deleting the joke', error: err });
+    })
+}
